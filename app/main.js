1ee9d000c08aa03107005eda0cc5a11c734e1388
Asama Qureshi <asama.qureshi@gmail.com>
2016-07-10 10:52:13 +1000

    refactor: using provider from react-redux

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './app';
 import store from './store/todos';
-import Provider from './Provider';
+import { Provider } from 'react-redux';
 
 ReactDom.render(
   <Provider store={store}>
