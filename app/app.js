1bf881017cd77281b4985d6bd02b044dea508bdc
Asama Qureshi <asama.qureshi@gmail.com>
2016-07-10 10:22:53 +1000

    refactor: removed unneed props from app and made into presenation component

--- app/app.js
+++ app/app.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import VisibleTodoList from './VisibleTodoList';
 import Footer from './Footer';
 import AddTodo from './AddTodo';
 
-export default class App extends React.Component {
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
 
-  constructor(props) {
-    super(props);
-    this.nextTodoId = 0;
-    this.state = {
-      newTodo: ''
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-      </div>
-    );
-  }
-
-}
+module.exports = App;
